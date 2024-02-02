d353ee918b17be3a731cc6583b0c30b4dc07a8c8
Black Diamond <149663797+MarPrieto7@users.noreply.github.com>
2024-02-02 13:18:53 +0100

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <p>Si soy el perfil que estas buscando</p>
+        <h2>Contáctame!</h2>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:mmarprietogarcia@gmail.com">mmarprietogarcia@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/mar-prieto-garcia">linkedin.com/in/mar-prieto-garcia</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/MarPrieto7">github.com/MarPrieto7</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
