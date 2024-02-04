4d13d06e9ceb5fe7fff638795a10ff6ba87d974b
Black Diamond <149663797+MarPrieto7@users.noreply.github.com>
2024-02-04 12:57:25 +0100

    Update Contact_Include_form_Container.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,95 @@
-import React from "react";
-
+// En tu archivo Contact.jsx
+import React, { useState } from "react";
 import styles from "./Contact.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Formulario enviado:", formData);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      message: "",
+    });
+  };
+
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <p>Si soy el perfil que estas buscando</p>
         <h2>Contáctame!</h2>
+      </div>
+      <div className={styles.formContainer}>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nombre:
+            <input
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Apellidos:
+            <input
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Teléfono:
+            <input
+              type="tel"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Mensaje:
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <button type="submit">Enviar</button>
+        </form>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
